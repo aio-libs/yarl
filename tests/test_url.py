125614753f31c6f663229bf236a55c0c1ea6cae5
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-08-02 16:37:28 +0300

    Add new props

--- tests/test_url.py
+++ tests/test_url.py
@@ -14,3 +14,53 @@ def test_str():
 def test_repr():
     url = URL('http://example.com')
     assert "URL('http://example.com')" == repr(url)
+
+
+def test_host():
+    url = URL('http://example.com')
+    assert "example.com" == url.host
+
+
+def test_host_when_port_is_specified():
+    url = URL('http://example.com:8888')
+    assert "example.com" == url.host
+
+
+def test_explicit_port():
+    url = URL('http://example.com:8888')
+    assert 8888 == url.port
+
+
+def test_implicit_port():
+    url = URL('http://example.com')
+    assert 80 == url.port
+
+
+def test_port_for_unknown_scheme():
+    url = URL('unknown://example.com')
+    assert url.port is None
+
+
+def test_scheme():
+    url = URL('http://example.com')
+    assert 'http' == url.scheme
+
+
+def test_path_empty():
+    url = URL('http://example.com')
+    assert '/' == url.path
+
+
+def test_path():
+    url = URL('http://example.com/path/to')
+    assert '/path/to' == url.path
+
+
+def test_user():
+    url = URL('http://user@example.com')
+    assert 'user' == url.user
+
+
+def test_password():
+    url = URL('http://user:password@example.com')
+    assert 'password' == url.password
--- yarl/__init__.py
+++ yarl/__init__.py
@@ -1,9 +1,20 @@
 __version__ = '0.0.1'
 
 
+from urllib.parse import urlsplit, urlunsplit
+
+
 # Why not furl?
 # Because I pretty sure URL class should be immutable
 # but furl is a mutable class.
+
+
+DEFAULT_PORTS = {
+    'http': 80,
+    'https': 443,
+    'ws': 80,
+    'wss': 443,
+}
 
 
 class URL:
@@ -13,10 +24,37 @@ class URL:
     # it's intended for libraries like aiohttp,
     # not to be passed into standard library functions like os.open etc.
     def __init__(self, val):
-        self._val = val
+        self._val = urlsplit(val)
 
     def __str__(self):
-        return self._val
+        return urlunsplit(self._val)
 
     def __repr__(self):
         return "{}('{}')".format(self.__class__.__name__, str(self))
+
+    @property
+    def host(self):
+        # Use host instead of hostname for sake of shortness
+        # May add .hostname prop later
+        return self._val.hostname
+
+    @property
+    def port(self):
+        return self._val.port or DEFAULT_PORTS.get(self._val.scheme)
+
+    @property
+    def scheme(self):
+        return self._val.scheme
+
+    @property
+    def path(self):
+        return self._val.path or '/'
+
+    @property
+    def user(self):
+        # not .username
+        return self._val.username
+
+    @property
+    def password(self):
+        return self._val.password
