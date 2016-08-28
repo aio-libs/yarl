76ebb7c1ddf8b7ac66ec60c36c65ccaba104ffa3
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-08-28 09:13:54 +0300

    Make the coverage 100%

--- tests/test_url.py
+++ tests/test_url.py
@@ -124,6 +124,11 @@ def test_hash():
     assert hash(URL('http://example.com/')) == hash(URL('http://example.com/'))
 
 
+def test_hash_double_call():
+    url = URL('http://example.com/')
+    assert hash(url) == hash(url)
+
+
 def test_le_less():
     url1 = URL('http://example1.com/')
     url2 = URL('http://example2.com/')
@@ -138,11 +143,23 @@ def test_le_eq():
     assert url1 <= url2
 
 
+def test_le_not_implemented():
+    url = URL('http://example1.com/')
+
+    assert url.__le__(123) is NotImplemented
+
+
 def test_lt():
     url1 = URL('http://example1.com/')
     url2 = URL('http://example2.com/')
 
     assert url1 < url2
+
+
+def test_lt_not_implemented():
+    url = URL('http://example1.com/')
+
+    assert url.__lt__(123) is NotImplemented
 
 
 def test_ge_more():
@@ -159,11 +176,23 @@ def test_ge_eq():
     assert url2 >= url1
 
 
+def test_ge_not_implemented():
+    url = URL('http://example1.com/')
+
+    assert url.__ge__(123) is NotImplemented
+
+
 def test_gt():
     url1 = URL('http://example1.com/')
     url2 = URL('http://example2.com/')
 
     assert url2 > url1
+
+
+def test_gt_not_implemented():
+    url = URL('http://example1.com/')
+
+    assert url.__gt__(123) is NotImplemented
 
 
 def test_clear_fragment_on_getting_parent():
