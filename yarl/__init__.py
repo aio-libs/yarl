78257e5e2e81f35b8d3473a44a9fa9910fa2a809
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-08-28 10:25:58 +0300

    Restore python 3.4 compatibility

--- yarl/__init__.py
+++ yarl/__init__.py
@@ -1,6 +1,6 @@
 from collections.abc import Mapping
 from urllib.parse import (SplitResult, SplitResultBytes, parse_qsl, quote,
-                          quote_plus, unquote, urlencode, urlsplit, urlunsplit)
+                          unquote, urlencode, urlsplit, urlunsplit)
 
 from multidict import MultiDict, MultiDictProxy
 
@@ -100,15 +100,8 @@ class URL:
         return quote(val.path).encode('ascii')
 
     @classmethod
-    def _encode_query_param(cls, val, safe='', encoding=None, errors=None):
-        val = val.replace('%', '%25')
-        return quote_plus(val, safe, encoding, errors)
-
-    @classmethod
     def _encode_query(cls, query):
-        ret = urlencode(query,
-                        quote_via=cls._encode_query_param)
-        return ret.encode('ascii')
+        return urlencode(query).encode('ascii')
 
     @classmethod
     def _encode_fragment(cls, val):
