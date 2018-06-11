398c18d6c829adea4b10fc956cd016f918b22aeb
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-06-11 13:12:25 +0300

    Fix #205: Drop Python 3.4 from trove classifiers

--- setup.py
+++ setup.py
@@ -82,7 +82,6 @@ args = dict(
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP'],
