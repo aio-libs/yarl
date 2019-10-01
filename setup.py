b175d3255ddaa05d7c5f750464376cf624bb7118
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-10-01 13:13:15 +0300

    Drop setup.py test obsolete command

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ import sys
 import re
 
 from setuptools import setup, Extension
-from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
-from distutils.command.build_ext import build_ext
 
 
 NO_EXTENSIONS = bool(os.environ.get("YARL_NO_EXTENSIONS"))  # type: bool
@@ -29,12 +27,6 @@ with fname.open(encoding="utf8") as fp:
         raise RuntimeError("Unable to determine version.")
 
 install_requires = ["multidict>=4.0", "idna>=2.0"]
-
-needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
-if needs_pytest:
-    setup_requires = ["pytest-runner"]
-else:
-    setup_requires = []
 
 
 def read(name):
@@ -65,8 +57,6 @@ args = dict(
     install_requires=install_requires,
     python_requires=">=3.5.3",
     include_package_data=True,
-    setup_requires=setup_requires,
-    tests_require=["pytest"],
 )
 
 
