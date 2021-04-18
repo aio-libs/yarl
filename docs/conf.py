c21655ea18c170b5d5515759ca420803a7853a81
Pierre-Louis Pécheux <33228518+ppecheux@users.noreply.github.com>
2021-04-19 00:35:41 +0200

    Delete the Azure Pipelines badge from the docs (#577)
    * delete azure badge
    
    * let template in comments

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ intersphinx_mapping = {
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
