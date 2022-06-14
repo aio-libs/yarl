1a60f709a19c9e5e8823689758a8bb0a720d095c
Martijn Pieters <github.com@zopatista.com>
2022-06-14 13:23:13 +0100

    Explicitly set a language (#739)
    All documentation is in English, and Sphinx 5 complains loudly if this isn't
    set.

--- docs/conf.py
+++ docs/conf.py
@@ -107,7 +107,7 @@ release = "{major}.{minor}.{patch}-{tag}".format(**_version_info)
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
