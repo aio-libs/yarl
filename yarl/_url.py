94b8679da05fd7b2d7bc86ed95e84c09f1946d75
dereckt <60044218+dereckt@users.noreply.github.com>
2022-12-12 15:30:25 -0500

    Fix to URL.with_port() input filtering (#793)
    * Handled case where update_query() is handed None as an argument. Added corresponding test file as well.
    
    * Added implementation and tests
    
    * Got rid of irrelevant test case for this branch
    
    * Cleanup
    
    * Readjusted to include 0 for ports (unsure if needed)
    
    * Create 793.bugfix.rst
    
    * Update 793.bugfix.rst
    
    Co-authored-by: Andrew Svetlov <andrew.svetlov@gmail.com>

--- yarl/_url.py
+++ yarl/_url.py
@@ -761,7 +761,7 @@ class URL:
             ret = cls._encode_host(host)
         else:
             ret = host
-        if port:
+        if port is not None:
             ret = ret + ":" + str(port)
         if password is not None:
             if not user:
@@ -869,8 +869,11 @@ class URL:
 
         """
         # N.B. doesn't cleanup query/fragment
-        if port is not None and not isinstance(port, int):
-            raise TypeError(f"port should be int or None, got {type(port)}")
+        if port is not None:
+            if isinstance(port, bool) or not isinstance(port, int):
+                raise TypeError(f"port should be int or None, got {type(port)}")
+            if port < 0 or port > 65535:
+                raise ValueError(f"port must be between 0 and 65535, got {port}")
         if not self.is_absolute():
             raise ValueError("port replacement is not allowed for relative URLs")
         val = self._val
