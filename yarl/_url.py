a845afb3d80e50db1013cd419dfa099f3ba2c56e
commonism <commonism@users.noreply.github.com>
2023-11-24 06:22:17 +0100

    Update yarl/_url.py
    Co-authored-by: Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>

--- yarl/_url.py
+++ yarl/_url.py
@@ -233,7 +233,7 @@ class URL:
             raise ValueError(
                 'Can\'t mix "authority" with "user", "password", "host" or "port".'
             )
-        if not isinstance(port, (int, None.__class__)):
+        if not isinstance(port, (int, type(None))):
             raise TypeError("port is required to be int")
         if port and not host:
             raise ValueError('Can\'t build URL with "port" but without "host".')
