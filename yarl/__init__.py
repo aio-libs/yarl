5644ca3c3daa07205b061e2376014c455505586e
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2023-12-06 02:37:57 +0100

    ⇪ Bump the version to v1.9.5.dev0

--- yarl/__init__.py
+++ yarl/__init__.py
@@ -1,5 +1,5 @@
 from ._url import URL, cache_clear, cache_configure, cache_info
 
-__version__ = "1.9.4"
+__version__ = "1.9.5.dev0"
 
 __all__ = ("URL", "cache_clear", "cache_configure", "cache_info")
