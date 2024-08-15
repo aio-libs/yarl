a93abd81d990a20d46fd278a7954ad92f6c91557
Markus Kötter <koetter@cispa.de>
2024-08-15 14:57:09 +0200

    as suggested

--- tests/test_url.py
+++ tests/test_url.py
@@ -1715,7 +1715,7 @@ def test_join_cpython_urljoin(base, url, expected):
 @pytest.mark.xfail(raises=TypeError, strict=True, reason="empty host name")
 def test_join_cpython_urljoin_fail():
     with pytest.raises(
-        TypeError, match="unsupported operand type\\(s\\) for \\+: 'NoneType' and 'str'"
+        TypeError, match=r"unsupported operand type\(s\) for \+: 'NoneType' and 'str'"
     ) as e:
         URL("http:///").join(URL(".."))
     raise e.value
