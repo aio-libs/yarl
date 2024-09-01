1844c33a9ef0a1cb4d7593c2ffd30fb4cfffaf2d
J. Nick Koston <nick@koston.org>
2024-08-31 17:23:23 -1000

    Update yarl/_helpers_c.pyi

--- yarl/_helpers_c.pyi
+++ yarl/_helpers_c.pyi
@@ -1,6 +1,6 @@
 from typing import Any
 
 class cached_property:
-    def __init__(self, wrapped: Any) -> None: ...  # pragma: no cover
-    def __get__(self, inst: Any, owner: Any) -> Any: ...  # pragma: no cover
-    def __set__(self, inst: Any, value: Any) -> None: ...  # pragma: no cover
+    def __init__(self, wrapped: Any) -> None: ...
+    def __get__(self, inst: Any, owner: Any) -> Any: ...
+    def __set__(self, inst: Any, value: Any) -> None: ...
