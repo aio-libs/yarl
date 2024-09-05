e0a379e0f85ee41cfedc056f46a0ceabf17abe01
J. Nick Koston <nick@koston.org>
2024-09-04 20:28:02 -1000

    Increment version to 1.9.12.dev0

--- yarl/__init__.py
+++ yarl/__init__.py
@@ -1,5 +1,5 @@
 from ._url import URL, cache_clear, cache_configure, cache_info
 
-__version__ = "1.9.11"
+__version__ = "1.9.12.dev0"
 
 __all__ = ("URL", "cache_clear", "cache_configure", "cache_info")
