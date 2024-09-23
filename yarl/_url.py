497752c16f0514afffb1ce17af21b63e19e98590
J. Nick Koston <nick@koston.org>
2024-09-23 13:37:45 -0500

    Restore unquoting of %2F in paths (#1151)

--- yarl/_url.py
+++ yarl/_url.py
@@ -225,7 +225,7 @@ class URL:
     _FRAGMENT_REQUOTER = _Quoter(safe="?/:@")
 
     _UNQUOTER = _Unquoter()
-    _PATH_UNQUOTER = _Unquoter(ignore="/", unsafe="+")
+    _PATH_UNQUOTER = _Unquoter(unsafe="+")
     _QS_UNQUOTER = _Unquoter(qs=True)
 
     _val: SplitResult
