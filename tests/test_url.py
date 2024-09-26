918da42060ef99be58320206874ed7dcd0595c38
J. Nick Koston <nick@koston.org>
2024-09-26 16:11:44 -0500

    more coverage

--- tests/test_url.py
+++ tests/test_url.py
@@ -181,6 +181,8 @@ def test_raw_host():
     [
         ("example.com"),
         ("[::1]"),
+        ("g%c3%bcnter.com"),
+        ("xn--gnter-4ya.com"),
     ],
 )
 def test_host_subcomponent(host: str):
@@ -197,6 +199,11 @@ def test_raw_host_non_ascii():
 def test_host_non_ascii():
     url = URL("http://оун-упа.укр")
     assert "оун-упа.укр" == url.host
+
+
+def test_host_non_ascii_percent_encoded():
+    url = URL("g%c3%bcnter.com")
+    assert url.host == "günter.com"
 
 
 def test_localhost():
