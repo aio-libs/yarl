f3d6a1b3149a6bf4989e1167a5c54501bc0966d6
J. Nick Koston <nick@koston.org>
2024-09-26 15:27:06 -0500

    Update tests/test_url_build.py

--- tests/test_url_build.py
+++ tests/test_url_build.py
@@ -22,7 +22,7 @@ def test_url_build_ipv6():
 
 def test_url_build_ipv6_brackets():
     u = URL.build(scheme="http", host="[::1]", encoded=True)
-    assert str(u) == "http://::1"
+    assert str(u) == "http://[::1]"
 
 
 def test_url_ipv4_in_ipv6():
