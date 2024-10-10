fe9659984da0a5ca309f0e4490da95f7c4b46f95
J. Nick Koston <nick@koston.org>
2024-10-10 13:54:01 -0500

    Add test coverage for handling port 0 (#1182)

--- tests/test_url.py
+++ tests/test_url.py
@@ -1486,6 +1486,15 @@ def test_is_default_port_for_unknown_scheme():
     assert not url.is_default_port()
 
 
+def test_handling_port_zero():
+    url = URL("http://example.com:0")
+    assert url.explicit_port == 0
+    assert url.explicit_port == url._val.port
+    assert url._port_not_default == 0
+    assert str(url) == "http://example.com:0"
+    assert not url.is_default_port()
+
+
 #
 
 
