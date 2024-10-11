dcfe9d1747ec6e55c81b446218dd422293babde5
J. Nick Koston <nick@koston.org>
2024-10-11 11:06:36 -0500

    more bench

--- url_benchmark.py
+++ url_benchmark.py
@@ -3,7 +3,7 @@ import timeit
 from yarl import URL
 
 print(
-    "Build URL with host and path: {:.3f} sec".format(
+    "Build URL with host and path and port: {:.3f} sec".format(
         timeit.timeit(
             "URL('http://localhost:1234/req')", globals={"URL": URL}, number=100000
         )
@@ -11,9 +11,43 @@ print(
 )
 
 print(
-    "Build URL with IP Address and path: {:.3f} sec".format(
+    "Build URL with host and path: {:.3f} sec".format(
+        timeit.timeit(
+            "URL('http://localhost/req')", globals={"URL": URL}, number=100000
+        )
+    )
+)
+
+
+print(
+    "Build URL with IPv4 Address and path and port: {:.3f} sec".format(
         timeit.timeit(
             "URL('http://127.0.0.1:1234/req')", globals={"URL": URL}, number=100000
         )
+    )
+)
+
+
+print(
+    "Build URL with IPv4 Address and path: {:.3f} sec".format(
+        timeit.timeit(
+            "URL('http://127.0.0.1/req')", globals={"URL": URL}, number=100000
+        )
+    )
+)
+
+
+print(
+    "Build URL with IPv6 Address and path and port: {:.3f} sec".format(
+        timeit.timeit(
+            "URL('http://[::1]:1234/req')", globals={"URL": URL}, number=100000
+        )
+    )
+)
+
+
+print(
+    "Build URL with IPv6 Address and path: {:.3f} sec".format(
+        timeit.timeit("URL('http://[::1]/req')", globals={"URL": URL}, number=100000)
     )
 )
