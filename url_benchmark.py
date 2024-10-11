2be4971d1a8c5147dd48c86b54f81e683f7bd92e
J. Nick Koston <nick@koston.org>
2024-10-11 17:05:55 -0500

    Add benchmarks for with_query (#1194)

--- url_benchmark.py
+++ url_benchmark.py
@@ -4,7 +4,7 @@ from yarl import URL
 
 MANY_HOSTS = [f"localhost.{i}" for i in range(10000)]
 MANY_URLS = [f"https://localhost.{i}" for i in range(10000)]
-
+BASE_URL = URL("http://localhost")
 print(
     "Build URL with host and path and port: {:.3f} sec".format(
         timeit.timeit(
@@ -120,5 +120,39 @@ print(
 print(
     "Make URL with IPv6 Address and path: {:.3f} sec".format(
         timeit.timeit("URL('http://[::1]/req')", globals={"URL": URL}, number=100000)
+    )
+)
+
+
+print(
+    "Make URL with query mapping: {:.3f} sec".format(
+        timeit.timeit(
+            "base_url.with_query("
+            "{'a':'1','b':'2','c':'3','d':'4','e':'5'"
+            ",'f':'6','g':'7','h':'8','i':'9','j':'10'}"
+            ")",
+            globals={"base_url": BASE_URL, "URL": URL},
+            number=100000,
+        )
+    )
+)
+
+
+print(
+    "Make URL with query sequence mapping: {:.3f} sec".format(
+        timeit.timeit(
+            "".join(
+                [
+                    "base_url.with_query({",
+                    *[
+                        f"'{i}':('1','2','3','4','5','6','7','8','9','10'),"
+                        for i in range(10)
+                    ],
+                    "})",
+                ]
+            ),
+            globals={"base_url": BASE_URL, "URL": URL},
+            number=100000,
+        )
     )
 )
