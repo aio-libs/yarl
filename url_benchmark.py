2b5b17419ab758bcba8b00adbdd8e8ccc9a3603f
J. Nick Koston <nick@koston.org>
2024-10-11 11:16:45 -0500

    cleanup

--- url_benchmark.py
+++ url_benchmark.py
@@ -5,13 +5,41 @@ from yarl import URL
 print(
     "Build URL with host and path and port: {:.3f} sec".format(
         timeit.timeit(
+            "URL.build(host='localhost', path='/req', port=1234)",
+            globals={"URL": URL},
+            number=100000,
+        )
+    )
+)
+
+print(
+    "Build URL with host: {:.3f} sec".format(
+        timeit.timeit(
+            "URL.build(host='localhost')", globals={"URL": URL}, number=100000
+        )
+    )
+)
+
+print(
+    "Build URL with host and port: {:.3f} sec".format(
+        timeit.timeit(
+            "URL.build(host='localhost', port=1234)",
+            globals={"URL": URL},
+            number=100000,
+        )
+    )
+)
+
+print(
+    "Make URL with host and path and port: {:.3f} sec".format(
+        timeit.timeit(
             "URL('http://localhost:1234/req')", globals={"URL": URL}, number=100000
         )
     )
 )
 
 print(
-    "Build URL with host and path: {:.3f} sec".format(
+    "Make URL with host and path: {:.3f} sec".format(
         timeit.timeit(
             "URL('http://localhost/req')", globals={"URL": URL}, number=100000
         )
@@ -20,7 +48,7 @@ print(
 
 
 print(
-    "Build URL with IPv4 Address and path and port: {:.3f} sec".format(
+    "Make URL with IPv4 Address and path and port: {:.3f} sec".format(
         timeit.timeit(
             "URL('http://127.0.0.1:1234/req')", globals={"URL": URL}, number=100000
         )
@@ -29,7 +57,7 @@ print(
 
 
 print(
-    "Build URL with IPv4 Address and path: {:.3f} sec".format(
+    "Make URL with IPv4 Address and path: {:.3f} sec".format(
         timeit.timeit(
             "URL('http://127.0.0.1/req')", globals={"URL": URL}, number=100000
         )
@@ -38,7 +66,7 @@ print(
 
 
 print(
-    "Build URL with IPv6 Address and path and port: {:.3f} sec".format(
+    "Make URL with IPv6 Address and path and port: {:.3f} sec".format(
         timeit.timeit(
             "URL('http://[::1]:1234/req')", globals={"URL": URL}, number=100000
         )
@@ -47,7 +75,7 @@ print(
 
 
 print(
-    "Build URL with IPv6 Address and path: {:.3f} sec".format(
+    "Make URL with IPv6 Address and path: {:.3f} sec".format(
         timeit.timeit("URL('http://[::1]/req')", globals={"URL": URL}, number=100000)
     )
 )
