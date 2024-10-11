1ec78d9ddbb3a22a6ae3ce18e5ecf1c5f525fda2
J. Nick Koston <nick@koston.org>
2024-10-11 14:46:35 -0500

    fix merge conflict error

--- url_benchmark.py
+++ url_benchmark.py
@@ -2,6 +2,9 @@ import timeit
 
 from yarl import URL
 
+MANY_HOSTS = [f"localhost.{i}" for i in range(10000)]
+MANY_URLS = [f"https://localhost.{i}" for i in range(10000)]
+
 print(
     "Build URL with host and path and port: {:.3f} sec".format(
         timeit.timeit(
@@ -13,9 +16,29 @@ print(
 )
 
 print(
+    "Build encoded URL with host and path and port: {:.3f} sec".format(
+        timeit.timeit(
+            "URL.build(host='localhost', path='/req', port=1234, encoded=True)",
+            globals={"URL": URL},
+            number=100000,
+        )
+    )
+)
+
+print(
     "Build URL with host: {:.3f} sec".format(
         timeit.timeit(
             "URL.build(host='localhost')", globals={"URL": URL}, number=100000
+        )
+    )
+)
+
+print(
+    "Build URL with different hosts: {:.3f} sec".format(
+        timeit.timeit(
+            "for host in hosts: URL.build(host=host)",
+            globals={"URL": URL, "hosts": MANY_HOSTS},
+            number=10,
         )
     )
 )
@@ -39,9 +62,29 @@ print(
 )
 
 print(
+    "Make encoded URL with host and path and port: {:.3f} sec".format(
+        timeit.timeit(
+            "URL('http://localhost:1234/req', encoded=True)",
+            globals={"URL": URL},
+            number=100000,
+        )
+    )
+)
+
+print(
     "Make URL with host and path: {:.3f} sec".format(
         timeit.timeit(
             "URL('http://localhost/req')", globals={"URL": URL}, number=100000
+        )
+    )
+)
+
+print(
+    "Make URL with many hosts: {:.3f} sec".format(
+        timeit.timeit(
+            "for url in urls: URL(url)",
+            globals={"URL": URL, "urls": MANY_URLS},
+            number=10,
         )
     )
 )
