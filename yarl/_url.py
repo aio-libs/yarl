9c444a71aafc271409cd84609d3f0b7b280dec6f
J. Nick Koston <nick@koston.org>
2024-10-12 13:34:27 -0500

    cleanup

--- yarl/_url.py
+++ yarl/_url.py
@@ -420,14 +420,11 @@ class URL:
         scheme, netloc, path, query, fragment = val
         if not val.path and self.absolute and (val.query or val.fragment):
             path = "/"
-        if (
-            explicit_port := self.explicit_port
-        ) is not None and explicit_port == self._default_port:
+        if (port := self.explicit_port) is not None and port == self._default_port:
             # port normalization - using None for default ports to remove from rendering
             # https://datatracker.ietf.org/doc/html/rfc3986.html#section-6.2.3
-            netloc = self._make_netloc(
-                self.raw_user, self.raw_password, self.host_subcomponent, None
-            )
+            host = self.host_subcomponent
+            netloc = self._make_netloc(self.raw_user, self.raw_password, host, None)
         return self._unsplit_result(scheme, netloc, path, query, fragment)
 
     @staticmethod
