a5e1bcc59376c0eed5156b613219deca46b8540e
J. Nick Koston <nick@koston.org>
2024-10-12 05:28:09 -0500

    Fix benchmarks including pytest overhead (#1215)

--- tests/test_quoting_benchmarks.py
+++ tests/test_quoting_benchmarks.py
@@ -1,5 +1,3 @@
-import pytest
-
 from yarl._quoting import _Quoter, _Unquoter
 
 QUOTER_SLASH_SAFE = _Quoter(safe="/")
@@ -7,25 +5,29 @@ QUOTER = _Quoter()
 UNQUOTER = _Unquoter()
 
 
-@pytest.mark.benchmark
-def test_quoter_ascii():
-    for _ in range(100):
-        QUOTER_SLASH_SAFE("/path/to")
+def test_quoter_ascii(benchmark):
+    @benchmark
+    def _run():
+        for _ in range(100):
+            QUOTER_SLASH_SAFE("/path/to")
 
 
-@pytest.mark.benchmark
-def test_quoter_pct():
-    for _ in range(100):
-        QUOTER("abc%0a")
+def test_quoter_pct(benchmark):
+    @benchmark
+    def _run():
+        for _ in range(100):
+            QUOTER("abc%0a")
 
 
-@pytest.mark.benchmark
-def test_quoter_quote():
-    for _ in range(100):
-        QUOTER("/шлях/файл")
+def test_quoter_quote(benchmark):
+    @benchmark
+    def _run():
+        for _ in range(100):
+            QUOTER("/шлях/файл")
 
 
-@pytest.mark.benchmark
-def test_unquoter():
-    for _ in range(100):
-        UNQUOTER("/path/to")
+def test_unquoter(benchmark):
+    @benchmark
+    def _run():
+        for _ in range(100):
+            UNQUOTER("/path/to")
