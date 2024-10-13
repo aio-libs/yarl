e43a7a30b5a7f1351aedd2b8ad3db1c6e44a4c6c
J. Nick Koston <nick@koston.org>
2024-10-13 11:47:04 -0500

    tweak

--- yarl/_url.py
+++ yarl/_url.py
@@ -1276,7 +1276,7 @@ class URL:
     @staticmethod
     def _query_var(v: QueryVariable) -> str:
         cls = type(v)
-        if cls is str or issubclass(cls, str):
+        if issubclass(cls, str):
             if TYPE_CHECKING:
                 assert isinstance(v, str)
             return v
