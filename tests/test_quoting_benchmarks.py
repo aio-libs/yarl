c6d46ae80e955244fcd0e31565f74632453f72d9
J. Nick Koston <nick@koston.org>
2024-10-15 16:34:08 -1000

    Add more quoting benchmarks (#1289)

--- tests/test_quoting_benchmarks.py
+++ tests/test_quoting_benchmarks.py
@@ -7,6 +7,18 @@ from yarl._quoting import _Quoter, _Unquoter
 QUOTER_SLASH_SAFE = _Quoter(safe="/")
 QUOTER = _Quoter()
 UNQUOTER = _Unquoter()
+QUERY_QUOTER = _Quoter(safe="?/:@", protected="=+&;", qs=True, requote=False)
+
+LONG_PATH = "/path/to" * 100
+LONG_QUERY = "a=1&b=2&c=3&d=4&e=5&f=6&g=7&h=8&i=9&j=0" * 25
+LONG_QUERY_WITH_PCT = LONG_QUERY + "&d=%25%2F%3F%3A%40%26%3B%3D%2B"
+
+
+def test_quote_query_string(benchmark: BenchmarkFixture) -> None:
+    @benchmark
+    def _run() -> None:
+        for _ in range(100):
+            QUERY_QUOTER("a=1&b=2&c=3&d=4&e=5&f=6&g=7&h=8&i=9&j=0")
 
 
 def test_quoter_ascii(benchmark: BenchmarkFixture) -> None:
@@ -16,11 +28,32 @@ def test_quoter_ascii(benchmark: BenchmarkFixture) -> None:
             QUOTER_SLASH_SAFE("/path/to")
 
 
+def test_quote_long_path(benchmark: BenchmarkFixture) -> None:
+    @benchmark
+    def _run() -> None:
+        for _ in range(100):
+            QUOTER(LONG_PATH)
+
+
 def test_quoter_pct(benchmark: BenchmarkFixture) -> None:
     @benchmark
     def _run() -> None:
         for _ in range(100):
             QUOTER("abc%0a")
+
+
+def test_long_query(benchmark: BenchmarkFixture) -> None:
+    @benchmark
+    def _run() -> None:
+        for _ in range(100):
+            QUERY_QUOTER(LONG_QUERY)
+
+
+def test_long_query_with_pct(benchmark: BenchmarkFixture) -> None:
+    @benchmark
+    def _run() -> None:
+        for _ in range(100):
+            QUERY_QUOTER(LONG_QUERY_WITH_PCT)
 
 
 def test_quoter_quote(benchmark: BenchmarkFixture) -> None:
