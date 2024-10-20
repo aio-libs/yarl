63059077667d7ff66c252e88b7d4ca12d50cfc51
J. Nick Koston <nick@koston.org>
2024-10-20 10:43:50 -1000

    pin versions

--- tests/test_url.py
+++ tests/test_url.py
@@ -1,3 +1,4 @@
+import sys
 from enum import Enum
 from urllib.parse import SplitResult, quote, unquote
 
@@ -306,6 +307,7 @@ def test_ipv6_missing_right_bracket():
         URL("http://[1dec:0:0:0::1/")
 
 
+@pytest.mark.skipif(sys.version_info < (3, 11), reason="requires Python 3.7 or higher")
 def test_ipv4_brackets_not_allowed():
     with pytest.raises(ValueError, match="An IPv4 address cannot be in brackets"):
         URL("http://[127.0.0.1]/")
@@ -2174,6 +2176,7 @@ def test_unsafe_url_bytes_are_removed(byte: str) -> None:
     assert str(url) == "http://example.com/"
 
 
+@pytest.mark.skipif(sys.version_info < (3, 11), reason="requires python3.11 or higher")
 @pytest.mark.parametrize("byte", tuple(_WHATWG_C0_CONTROL_OR_SPACE))
 def test_control_chars_are_removed(byte: str) -> None:
     url = URL(f"{byte}http://example.com/")
