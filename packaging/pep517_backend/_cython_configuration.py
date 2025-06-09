872a5951a37ac7c58808aae5148bb1676e115edb
J. Nick Koston <nick@koston.org>
2025-06-09 15:20:20 -0500

    Disable linetrace by default in pyproject.toml
    Only turn it on when we build with line tracing

--- packaging/pep517_backend/_cython_configuration.py
+++ packaging/pep517_backend/_cython_configuration.py
@@ -18,7 +18,7 @@ from ._transformers import get_cli_kwargs_from_config, get_enabled_cli_flags_fro
 class Config(TypedDict):
     env: dict[str, str]
     flags: dict[str, bool]
-    kwargs: dict[str, str]
+    kwargs: dict[str, str | dict[str, str]]
     src: list[str]
 
 
@@ -79,10 +79,18 @@ def get_local_cython_config() -> Config:
     return config_mapping['tool']['local']['cythonize']  # type: ignore[no-any-return]
 
 
-def make_cythonize_cli_args_from_config(config: Config) -> list[str]:
+def make_cythonize_cli_args_from_config(config: Config, cython_line_tracing_requested: bool = False) -> list[str]:
     py_ver_arg = f'-{_python_version_tuple.major!s}'
 
     cli_flags = get_enabled_cli_flags_from_config(config['flags'])
+
+    # If line tracing is requested, add it to the directives
+    if cython_line_tracing_requested:
+        directives = config['kwargs'].setdefault('directive', {})
+        assert isinstance(directives, dict)  # Type narrowing for mypy
+        directives['linetrace'] = 'True'
+        directives['profile'] = 'True'
+
     cli_kwargs = get_cli_kwargs_from_config(config['kwargs'])
 
     return cli_flags + [py_ver_arg] + cli_kwargs + ['--'] + config['src']
