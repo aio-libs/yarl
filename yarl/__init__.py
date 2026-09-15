3dc4b26db6be0cca8ae5d3ed0b85eb73479ff052
J. Nick Koston <nick@koston.org>
2026-09-15 11:31:20 -0500

    Release 1.25.0 (#1905)

--- yarl/__init__.py
+++ yarl/__init__.py
@@ -2,7 +2,7 @@ from ._parse import query_to_pairs
 from ._query import Query, QueryVariable, SimpleQuery
 from ._url import URL, cache_clear, cache_configure, cache_info
 
-__version__ = "1.24.5"
+__version__ = "1.25.0"
 
 __all__ = (
     "URL",
